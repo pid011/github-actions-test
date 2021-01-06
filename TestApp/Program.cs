27674e066526d505dd4646da78ce3414d3ba47fc
Pilgyeong Gwak <pid011@kakao.com>
2021-01-07 06:07:30 +0900

    Update Program.cs

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -6,5 +6,17 @@ internal class Program
     {
         Console.WriteLine(args[0]);
         Console.WriteLine($"arg length is {args[0].Length}");
+
+        Console.WriteLine();
+
+        string secrets = Environment.GetEnvironmentVariable("TEST_KEY", EnvironmentVariableTarget.Process);
+        if (secrets is not null)
+        {
+            Console.WriteLine($"env length is {secrets.Length}");
+        }
+        else
+        {
+            Console.WriteLine("Cannot found env...");
+        }
     }
 }
