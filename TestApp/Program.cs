6797ae3236a46a16869cedd03a383081a07d3d21
Pilgyeong Gwak <pid011@kakao.com>
2021-01-07 06:02:10 +0900

    Update Program.cs

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -5,5 +5,6 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine(args[0]);
+        Console.WriteLine($"arg length is {args[0].Length}");
     }
 }
